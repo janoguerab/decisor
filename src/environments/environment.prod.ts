3763f1c62028fc8f2ad0a3d405b1a84279f012f3
Unknown <janoguerab@unal.edu.co>
2017-11-22 04:09:02 -0500

    enviroments deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig : {
+    apiKey: "AIzaSyCsB5lwlRGUQnHhzFk8KbbEe7CxMxcQnXw",
+    authDomain: "decisor-ccd89.firebaseapp.com",
+    databaseURL: "https://decisor-ccd89.firebaseio.com",
+    projectId: "decisor-ccd89",
+    storageBucket: "decisor-ccd89.appspot.com",
+    messagingSenderId: "537151832864"
+  }
 };
